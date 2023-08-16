14c661420b50be89bfd15f32db7bacc9a534b444
psychofizz <psychofizz@pm.me>
2023-08-15 21:53:29 -0600

    Limpieza de dependencias

--- src/main/java/hn/unah/lenguajes/chinese/models/Empleados.java
+++ src/main/java/hn/unah/lenguajes/chinese/models/Empleados.java
@@ -1,7 +1,6 @@
 package hn.unah.lenguajes.chinese.models;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
